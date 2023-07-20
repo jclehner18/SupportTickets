9f64b8e95e3e65a8ec9399e3a7efe11933c9bd29
Jack Lehner <jacklehner44@yahoo.com>
2023-07-20 14:39:31 -0400

    frontend started. home, login, register pages setup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+import {toast} from 'react-toastify'
+
+
+function Login() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const {email, password} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+      
+    }
+
+
+  return (
+    <>
+        <section className="heading">
+            <h1>
+                <FaSignInAlt /> Login
+            </h1>
+            <p>Please login to get support</p>
+        </section>
+        <section className="form">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input type="email" className="form-control" id='email' value={email} onChange={onChange} placeholder='Enter your email' name={email} required/>
+                </div>
+                <div className="form-group">
+                    <input type="password" className="form-control" id='password' value={password} onChange={onChange} placeholder='Enter your password' name={password} required/>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-block" type='submit'>Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
+  )
+}
+
+export default Login
