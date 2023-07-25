5565025675b808acc18328d6455cebd8207af8c2
Jack Lehner <jacklehner44@yahoo.com>
2023-07-24 20:14:56 -0400

    heroku setup in serverjs

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
+const path = require('path')
 
 
 //connect to db
@@ -14,12 +15,24 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.send('Welcome to Support Desk')
 })
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//serve frontend
+if(process.env.NODE_ENV === 'production') {
+    //set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.send('Welcome to Support Desk')
+    })
+}
 
 app.use(errorHandler)
 
